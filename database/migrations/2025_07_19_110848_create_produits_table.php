ba04be063789a1e0687834b3da0457d5b6bccccb
e-mandy <andymfrd02@gmail.com>
2025-07-19 13:00:40 +0100

    Mise en place des migrations (1/2)

---
+++ database/migrations/2025_07_19_110848_create_produits_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom', 255);
+            $table->text('description');
+
+            //Le prix ne doit pas dépasser 7 chiffres au total et ne doit pas dépasser 2 chiffres après la virgule
+            $table->decimal('prix', 7, 2);
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produits');
+    }
+};
