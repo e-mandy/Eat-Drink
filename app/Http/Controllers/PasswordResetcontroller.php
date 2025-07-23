904ccf0bdceb0dbecb51e74fae665558ea57a59c
Housniyath ADJIBADE <jenniferadjibade@gmail.com>
2025-07-23 15:31:36 +0100

    Suppression des champs prenom et nom_entreprise dans users

---
+++ app/Http/Controllers/PasswordResetcontroller.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Password;
+
+use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Support\Str;
+
+class PasswordResetcontroller extends Controller
+{
+     // Affiche le formulaire de demande de lien
+    public function showLinkRequestForm()
+    {
+        return view('auth.passwords.email');
+    }
+
+    // Envoie l'e-mail avec le lien de réinitialisation
+    public function sendResetLinkEmail(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status === Password::RESET_LINK_SENT
+            ? back()->with(['status' => __($status)])
+            : back()->withErrors(['email' => __($status)]);
+    }
+
+    // Affiche le formulaire de réinitialisation
+    public function showResetForm($token)
+    {
+        return view('auth.passwords.reset', ['token' => $token]);
+    }
+
+    // Traitement de la réinitialisation
+    public function reset(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password),
+                    'remember_token' => Str::random(60),
+                ])->save();
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+            ? redirect()->route('connexion.form')->with('status', __($status))
+            : back()->withErrors(['email' => [__($status)]]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;  
 
+use App\Http\Controllers\PasswordResetController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -13,7 +15,15 @@ Route::get('/inscription', function () {
     return view('inscription');
 })->name('inscription.form');
 
+Route::get('/mot-de-passe-oublie', [PasswordResetController::class, 'showLinkRequestForm'])->name('password.request');
+
+Route::get('/reset-password/{token}', [PasswordResetController::class, 'showResetForm'])->name('password.reset');
+
 // Route pour la connexion
 Route::post('/connexion',[AuthController::class,'connexion'])->name('connexion.submit');
 //Route pour l'inscription
 Route::post('/inscription',[AuthController::class,'inscription'])->name('inscription.submit');
+//Route pour mot de passe et email
+Route::post('/mot-de-passe-email', [PasswordResetController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::post('/reset-password', [PasswordResetController::class, 'reset'])->name('password.update');
+
